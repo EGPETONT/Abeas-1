86c19339fa677eacfbc90a242cfd3bd7005fd5d7
LuckyMonK <klimentpereguda@gmail.com>
2021-02-10 12:07:39 +0300

    [dev] first commit

---
+++ Assets/Scripts/PlayerFormController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerFormController : MonoBehaviour
+{
+    [SerializeField] private GameObject[] forms;
+
+    private int currentState = 0;
+    public void NextState() {
+        DeactiveAll();
+        currentState++;
+        if (currentState >= forms.Length) {
+            currentState = 0;
+        }
+        ActivateState(currentState);
+    }
+
+    private void ActivateState(int state) {
+        forms[state].SetActive(true);
+    }
+
+    private void DeactiveAll() {
+        foreach (var go in forms) {
+            go.SetActive(false);
+        }
+    }
+}
---
+++ Assets/Scripts/TestAnimController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestAnimController : MonoBehaviour
+{
+    [SerializeField] private Animation[] anim;
+    [SerializeField] private Vector3 currectPosition;
+    [SerializeField] private Transform lastAnimatedObject;
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A)) {
+            anim[0].Play();
+            lastAnimatedObject = anim[0].transform;
+
+        } else if (Input.GetKeyDown(KeyCode.S))
+        {
+            Debug.Log(anim[1].transform.position + " " + lastAnimatedObject.position);
+            anim[1].transform.root.position += lastAnimatedObject.position;
+            anim[1].Play();
+        }
+        else if(Input.GetKeyDown(KeyCode.D)) {
+            anim[2].Play();
+        }
+    }
+
+    private void SetCurrentPosition(Transform targetPos) { 
+        
+    }
+}
