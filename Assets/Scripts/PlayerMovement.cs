b39e4fd38eff07779d95baffda275bea3b7ffade
LuckyMonK <klimentpereguda@gmail.com>
2021-02-19 13:08:14 +0300

    [dev] movement and camera updates. new menu. rendering settings updates

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,65 +7,101 @@ using Lean.Gui;
 //[RequireComponent(typeof(CapsuleCollider))]
 public class PlayerMovement : MonoBehaviour
 {
-	public float speed = 10.0f;
-	public float gravity = 10.0f;
-	public float maxVelocityChange = 10.0f;
-	public bool canJump = true;
-	public float jumpHeight = 2.0f;
-	private bool grounded = false;
+	//public float speed = 10.0f;
+	//public float gravity = 10.0f;
+	//public float maxVelocityChange = 10.0f;
+	//public bool canJump = true;
+	//public float jumpHeight = 2.0f;
+	//private bool grounded = false;
 
-	[SerializeField] private Rigidbody rigidbody;
+	//[SerializeField] private Rigidbody rigidbody;
+	//[SerializeField] private LeanJoystick joystick;
+	//[SerializeField] private Transform root;
+	//void Awake()
+	//{
+	//	rigidbody.freezeRotation = true;
+	//	rigidbody.useGravity = false;
+	//}
+
+	//void FixedUpdate()
+	//{
+	//	if (grounded)
+	//	{
+	//		// Calculate how fast we should be moving
+	//		//Vector3 targetVelocity = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+	//		Vector3 targetVelocity = new Vector3(joystick.ScaledValue.x, 0, joystick.ScaledValue.y);
+	//		targetVelocity = transform.TransformDirection(targetVelocity);
+	//		targetVelocity *= speed;
+
+	//		// Apply a force that attempts to reach our target velocity
+	//		Vector3 velocity = rigidbody.velocity;
+	//		Vector3 velocityChange = (targetVelocity - velocity);
+	//		velocityChange.x = Mathf.Clamp(velocityChange.x, -maxVelocityChange, maxVelocityChange);
+	//		velocityChange.z = Mathf.Clamp(velocityChange.z, -maxVelocityChange, maxVelocityChange);
+	//		velocityChange.y = 0;
+	//		rigidbody.AddForce(velocityChange, ForceMode.VelocityChange);
+
+	//		// Jump
+	//		if (canJump && Input.GetButton("Jump"))
+	//		{
+	//			rigidbody.velocity = new Vector3(velocity.x, CalculateJumpVerticalSpeed(), velocity.z);
+	//		}
+
+	//		//root.Rotate(0.0f, -joystick.ScaledValue.x, 0.0f);
+	//		root.eulerAngles = new Vector3(0.0f, -joystick.ScaledValue.x * 90, 0.0f);
+	//	}
+
+	//	// We apply gravity manually for more tuning control
+	//	rigidbody.AddForce(new Vector3(0, -gravity * rigidbody.mass, 0));
+
+	//	grounded = false;
+	//}
+
+	//void OnCollisionStay()
+	//{
+	//	grounded = true;
+	//}
+
+	//float CalculateJumpVerticalSpeed()
+	//{
+	//	// From the jump height and gravity we deduce the upwards speed 
+	//	// for the character to reach at the apex.
+	//	return Mathf.Sqrt(2 * jumpHeight * gravity);
+	//}
+
+
+	public float walkSpeed = 6;
+
+	Vector3 moveAmount;
+	Vector3 smoothMoveVelocity;
+	Rigidbody rb;
+
 	[SerializeField] private LeanJoystick joystick;
+
 	[SerializeField] private Transform root;
 	void Awake()
 	{
-		rigidbody.freezeRotation = true;
-		rigidbody.useGravity = false;
+		rb = GetComponent<Rigidbody>();
+	}
+
+	void Update()
+	{
+
+		float inputX = joystick.ScaledValue.x;//Input.GetAxisRaw("Horizontal");
+		float inputY = joystick.ScaledValue.y; //Input.GetAxisRaw("Vertical");
+
+		Vector3 moveDir = new Vector3(inputX, 0, inputY).normalized;
+		Vector3 targetMoveAmount = moveDir * walkSpeed;
+		moveAmount = Vector3.SmoothDamp(moveAmount, targetMoveAmount, ref smoothMoveVelocity, .15f);
+
+		//root.transform.rotation = Quaternion.LookRotation(Vector3.zero);
+		//root.transform.Rotate(-90, 0, 0);
 	}
 
 	void FixedUpdate()
 	{
-		if (grounded)
-		{
-			// Calculate how fast we should be moving
-			//Vector3 targetVelocity = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-			Vector3 targetVelocity = new Vector3(joystick.ScaledValue.x, 0, joystick.ScaledValue.y);
-			targetVelocity = transform.TransformDirection(targetVelocity);
-			targetVelocity *= speed;
 
-			// Apply a force that attempts to reach our target velocity
-			Vector3 velocity = rigidbody.velocity;
-			Vector3 velocityChange = (targetVelocity - velocity);
-			velocityChange.x = Mathf.Clamp(velocityChange.x, -maxVelocityChange, maxVelocityChange);
-			velocityChange.z = Mathf.Clamp(velocityChange.z, -maxVelocityChange, maxVelocityChange);
-			velocityChange.y = 0;
-			rigidbody.AddForce(velocityChange, ForceMode.VelocityChange);
-
-			// Jump
-			if (canJump && Input.GetButton("Jump"))
-			{
-				rigidbody.velocity = new Vector3(velocity.x, CalculateJumpVerticalSpeed(), velocity.z);
-			}
-
-			//root.Rotate(0.0f, -joystick.ScaledValue.x, 0.0f);
-			root.eulerAngles = new Vector3(0.0f, -joystick.ScaledValue.x * 90, 0.0f);
-		}
-
-		// We apply gravity manually for more tuning control
-		rigidbody.AddForce(new Vector3(0, -gravity * rigidbody.mass, 0));
-
-		grounded = false;
-	}
-
-	void OnCollisionStay()
-	{
-		grounded = true;
-	}
-
-	float CalculateJumpVerticalSpeed()
-	{
-		// From the jump height and gravity we deduce the upwards speed 
-		// for the character to reach at the apex.
-		return Mathf.Sqrt(2 * jumpHeight * gravity);
+		Vector3 localMove = transform.TransformDirection(moveAmount) * Time.fixedDeltaTime;
+		rb.MovePosition(rb.position + localMove);
 	}
 }
---
+++ Assets/Scripts/SphereCamera.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SphereCamera : MonoBehaviour
+{
+    [SerializeField] private Vector3 sphereCenter;
+    [SerializeField] private Transform player;
+    [SerializeField] private float cameraSpeed = 10f;
+
+    private void Update()
+    {
+        transform.LookAt(player.position);
+        //transform.eulerAngles = new Vector3(transform.eulerAngles.x, transform.eulerAngles.z, transform.eulerAngles.z);
+        transform.position = Vector3.Lerp(transform.position, player.position + player.up * 80f, Time.deltaTime * cameraSpeed);
+    }
+}
